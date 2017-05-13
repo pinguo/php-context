fa3a04cdcb1d90752ec71b5d4ffe7aeb80d16da7
pinguo-xudianyang <xudianyang@camera360.com>
2017-05-13 17:17:16 +0800

    * get for null

--- src/AbstractContext.php
+++ src/AbstractContext.php
@@ -25,16 +25,61 @@ abstract class AbstractContext implements \ArrayAccess
     /**
      * @var string
      */
-    public $logId;
+    protected $logId;
 
     /**
      * @var PGLog
      */
-    public $PGLog;
+    protected $PGLog;
 
     public function __sleep()
     {
         return ['logId'];
+    }
+
+    /**
+     * 设置日志对象
+     *
+     * @param $log
+     * @return $this
+     */
+    public function setLog($log)
+    {
+        $this->PGLog = $log;
+        return $this;
+    }
+
+    /**
+     * 获取日志对象
+     *
+     * @return PGLog
+     */
+    public function getLog()
+    {
+        return $this->PGLog ?? null;
+    }
+
+
+    /**
+     * 设置日志ID
+     *
+     * @param $logId
+     * @return $this
+     */
+    public function setLogId($logId)
+    {
+        $this->logId = $logId;
+        return $this;
+    }
+
+    /**
+     * 获取日志ID
+     *
+     * @return string
+     */
+    public function getLogId()
+    {
+        return $this->logId ?? null;
     }
 
     /**
